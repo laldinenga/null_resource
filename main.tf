f5f5e84eb76eba7214e3d151b89ba4a5a108bafb
Zsanga <105400251+Zothans@users.noreply.github.com>
2023-06-28 14:18:37 +1000

    Update main.tf add test 10

--- main.tf
+++ main.tf
@@ -9,7 +9,7 @@ terraform {
 
 provider "null" {
 }
-resource "null_resource" "test" {}
+resource "null_resource" "test10" {}
 resource "null_resource" "test2" {}
 resource "null_resource" "test3" {}
 resource "null_resource" "test4" {}
