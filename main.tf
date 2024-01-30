503a78a0179ef706eafdc1c7c7a34651b6aadf6f
Laldinenga <110852005+laldinenga@users.noreply.github.com>
2024-01-30 16:30:43 +1100

    delete 3 and 4

--- main.tf
+++ main.tf
@@ -13,5 +13,4 @@ provider "null" {
 resource "null_resource" "test" {}
 resource "null_resource" "test1" {}
 resource "null_resource" "test2" {}
-resource "null_resource" "test3" {}
-resource "null_resource" "test4" {}
+
